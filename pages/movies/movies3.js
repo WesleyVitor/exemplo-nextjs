a53d06e98d88f06b8e2226aa109e2be561b7983b
WesleyVitor <wesleydemorais@outlook.com.br>
2022-02-04 13:43:05 -0300

    add: input to search movie

--- pages/movies/movies3.js
+++ pages/movies/movies3.js
@@ -9,26 +9,42 @@ export default function Movies3() {
     const [url, setUrl] = useState("");
     const [title, setTitle] = useState("");
     const [hidden, setHidden] = useState(true);
+    const [filme, setFilme] = useState("");
     const { data, error } = useSWR(url, fetcher);
 
-    const onClickHandler = (e) => {
-        e.preventDefault();
-
+    const handleSubmit = () => {
+        setHidden(true);
         if (url === "")
-            setUrl("http://www.omdbapi.com/?apikey=9edb7018&s=bagdad");
+            setUrl(
+                `http://www.omdbapi.com/?apikey=9edb7018&s=${
+                    filme ? filme : "bagdad"
+                }`
+            );
         else setUrl("");
     };
 
     const handleClickDescription = (title) => {
         setTitle(title);
-        setHidden(false);
+        setHidden(!hidden);
+    };
+
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setFilme(value);
     };
 
     return (
         <>
             <div>
-                <TheLink url={url} handler={onClickHandler} />
-
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleSubmit();
+                    }}
+                >
+                    <input type="text" name="filme" onChange={handleChange} />
+                    <input type="submit" value="Pesquisar" />
+                </form>
                 <TheMovies
                     handleClickDescription={handleClickDescription}
                     data={
